e86f993b3bfd4634add40345d82a63dbd08543d7
yoozze <joze.peternelj@qlector.com>
2024-11-06 19:00:21 +0100

    Updated JSON report

--- services/cms/src/modules/custom/public_api/src/Plugin/rest/resource/PublicApiResource.php
+++ services/cms/src/modules/custom/public_api/src/Plugin/rest/resource/PublicApiResource.php
@@ -42,30 +42,72 @@ class PublicApiResource extends ResourceBase {
         ];
         $content_types = \Drupal::entityTypeManager()->getStorage('node_type')->loadMultiple();
 
+        // Get url parameter 'from' timestamp.
+        $from = \Drupal::request()->query->get('from');
+        if ($from) {
+            $from = (int) $from;
+        } else {
+            $from = 0;
+        }
+
+        // Get url parameter 'to' timestamp.
+        $to = \Drupal::request()->query->get('to');
+        if ($to) {
+            $to = (int) $to;
+        } else {
+            $to = time();
+        }
+
         foreach ($content_type_ids as $content_type_id) {
             $content_type = $content_types[$content_type_id];
 
-            // Count the nodes.
+            // Count nodes.
             $count = \Drupal::entityQuery('node')
                 ->accessCheck(FALSE)
                 ->condition('type', $content_type_id)
                 ->count()
                 ->execute();
 
-            // Get the latest nodes.
+            // Get nodes.
             $query = \Drupal::entityQuery('node')
                 ->accessCheck(FALSE)
                 ->condition('type', $content_type_id)
+                ->condition('created', $from, '>=')
+                ->condition('created', $to, '<=')
                 ->sort('created', 'DESC')
-                ->range(0, 5)
+                // ->range(0, 5)
                 ->execute();
             $nodes = Node::loadMultiple($query);
 
-            $latest = [];
+            $items = [];
             foreach ($nodes as $node) {
-                $latest[] = [
+
+                // Get country.
+                $country = NULL;
+                $country_id = $node->get('field_country')->target_id;
+                if ($country_id) {
+                    $country_term = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->load($country_id);
+                    if ($country_term) {
+                        $country = $country_term->get('name')->value;
+                    }
+                }
+
+                // Get area of action items.
+                $areas_of_action = [];
+                $areas_of_action_ids = $node->get('field_area_of_action')->getValue();
+                foreach ($areas_of_action_ids as $area_of_action_id) {
+                    $area_of_action_term = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->load($area_of_action_id['target_id']);
+                    if ($area_of_action_term) {
+                        $areas_of_action[] = $area_of_action_term->get('name')->value;
+                    }
+                }
+
+                $items[] = [
                     'id' => $node->id(),
                     'title' => $node->getTitle(),
+                    'description' => $node->get('field_description')->value,
+                    'country' => $country,
+                    'area_of_action' => $areas_of_action,
                     'created' => $node->getCreatedTime(),
                     'url' => $node->toUrl()->setAbsolute()->toString(),
                 ];
@@ -75,7 +117,7 @@ class PublicApiResource extends ResourceBase {
                 'type' => $content_type->id(),
                 'name' => $content_type->label(),
                 'count' => $count,
-                'latest' => $latest,
+                'items' => $items,
             ];
         }
 
