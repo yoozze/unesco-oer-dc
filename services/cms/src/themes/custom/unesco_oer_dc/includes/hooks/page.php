773a506562fb3fd5eea5b9ca95ecfc6a83f77599
yoozze <joze.peternelj@qlector.com>
2024-11-19 03:10:37 +0100

    Added observatory template

--- services/cms/src/themes/custom/unesco_oer_dc/includes/hooks/page.php
+++ services/cms/src/themes/custom/unesco_oer_dc/includes/hooks/page.php
@@ -1,8 +1,28 @@
 <?php
 
+use Drupal\Core\Url;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
 /**
  * Implements theme hooks for page.
  */
+
+function unesco_oer_dc_theme_suggestions_page_alter(&$suggestions, &$variables) {
+    // Get path
+    $path = \Drupal::service('path.current')->getPath();
+
+    // Get URL alias
+    $alias = \Drupal::service('path_alias.manager')->getAliasByPath($path);
+
+    // Get slug
+    $path_items = explode('/', trim($alias, '/'));
+    $slug = end($path_items);
+    $variables['slug'] = $slug;
+
+    // Add page suggestion
+    $suggestions[] = 'page__' . clean_suggetion($slug);
+}
 
 function unesco_oer_dc_preprocess_page(&$variables) {
     // $classes = &get_classes($variables);
@@ -36,6 +56,41 @@ function unesco_oer_dc_preprocess_page(&$variables) {
             '#sorted' => true,
             '#theme_wrappers' => ['region'],
             '#site_name' => \Drupal::config('system.site')->get('name')
+        ];
+    }
+
+    // Get path
+    $path = \Drupal::service('path.current')->getPath();
+
+    // Get URL alias
+    $alias = \Drupal::service('path_alias.manager')->getAliasByPath($path);
+
+    // Get slug
+    $path_items = explode('/', trim($alias, '/'));
+    $slug = end($path_items);
+    $variables['slug'] = $slug;
+
+    // Get additional data for specific pages
+    if ($slug === 'observatory') {
+        // Get name of areas_of_action vocabulary
+        $vocabulary = \Drupal::entityTypeManager()->getStorage('taxonomy_vocabulary')->load('areas_of_action');
+
+        // Get all items of areas_of_action taxonomy
+        $terms = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree('areas_of_action');
+        $variables['areas_of_action'] = [
+            'vocabulary' => $vocabulary,
+            'terms' => $terms
+        ];
+
+        // Get area and view from query string
+        $area = max(1, min(5, intval(\Drupal::request()->query->get('area') ?? '1')));
+        $view = \Drupal::request()->query->get('view');
+        if (!in_array($view, ['news', 'dashboard'])) {
+            $view = 'news';
+        }
+        $variables['get'] = [
+            'area' => $area,
+            'view' => $view
         ];
     }
 }
