395413b50e6cf3be1df86bfb0798cfc329a73a4a
yoozze <joze.peternelj@qlector.com>
2025-01-03 01:55:44 +0100

    Updated join link

--- services/cms/src/themes/custom/unesco_oer_dc/includes/hooks/menu.php
+++ services/cms/src/themes/custom/unesco_oer_dc/includes/hooks/menu.php
@@ -4,9 +4,8 @@
  * Implements theme hooks for menu.
  */
 
-// function unesco_oer_dc_preprocess_menu(&$variables) {
-//     $classes = &$variables['attributes']['class'];
-
-//     $classes[] = 'c-navigation__menu';
-//     $classes[] = 'c-navigation__menu--' . $variables['menu_name'];
-// }
+function unesco_oer_dc_preprocess_menu__account(&$variables) {
+    if (\Drupal::currentUser()->isAnonymous()) {
+        $variables['join_link'] = get_join_link();
+    }
+}
