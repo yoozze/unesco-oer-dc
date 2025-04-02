25f2c1f25ec07f13d0fdaa906e62fb09c6454670
yoozze <joze.peternelj@qlector.com>
2025-04-02 08:55:25 +0200

    Automatically embed youtube videos in resources

--- services/cms/src/themes/custom/unesco_oer_dc/includes/hooks/node.php
+++ services/cms/src/themes/custom/unesco_oer_dc/includes/hooks/node.php
@@ -4,6 +4,20 @@
  * Implements theme hooks for node.
  */
 
+function get_youtube_embed($url, $width = 560, $height = 315) {
+    $url = str_replace('watch?v=', 'embed/', $url);
+    $url = str_replace('live/', 'embed/', $url);
+    $embed = '<iframe';
+    $embed .= ' width="' . $width . '"';
+    $embed .= ' height="' . $height . '"';
+    $embed .= ' src="' . $url . '"';
+    $embed .= ' frameborder="0"';
+    $embed .= ' allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"';
+    $embed .= ' allowfullscreen';
+    $embed .= '></iframe>';
+    return $embed;
+}
+
 function unesco_oer_dc_theme_suggestions_node_alter(&$suggestions, &$variables) {
     $node_name = get_node_name($variables['elements']['#attributes']['data-history-node-id']);
     if (!empty($node_name)) {
@@ -14,12 +28,17 @@ function unesco_oer_dc_theme_suggestions_node_alter(&$suggestions, &$variables) 
 /**
  * Implements hook_preprocess_HOOK() for node.html.twig.
  */
-// function unesco_oer_dc_preprocess_node(&$variables) {
-//     $classes = &get_classes($variables);
+function unesco_oer_dc_preprocess_node(&$variables) {
 
-//     $node_name = get_node_name($variables['elements']['#attributes']['data-history-node-id']);
-//     if (!empty($node_name)) {
-//         $classes[] = 'c-node--' . $node_name;
-//         $variables['theme_hook_suggestions'][] = 'node__' . $node_name;
-//     }
-// }
+    $node = $variables['node'];
+    $node_type = $node->getType();
+
+    if ($node_type === 'resource') {
+        $url = $node->get('field_url')->uri;
+
+        // If url is linking youtube video, prepare embed html
+        if (!empty($url) && strpos($url, 'https://www.youtube.com') !== false) {
+            $variables['video_embed'] = get_youtube_embed($url, 560 * 2, 315 * 2);
+        }
+    }
+}
