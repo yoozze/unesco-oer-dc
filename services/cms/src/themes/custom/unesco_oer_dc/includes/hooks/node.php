bdc834f7e2d22672d7b12366410e3acd650be2c1
yoozze <joze.peternelj@qlector.com>
2025-05-05 01:51:26 +0200

    Added avatar to posts

--- services/cms/src/themes/custom/unesco_oer_dc/includes/hooks/node.php
+++ services/cms/src/themes/custom/unesco_oer_dc/includes/hooks/node.php
@@ -1,5 +1,8 @@
 <?php
 
+use Drupal\Core\Url;
+use Drupal\image\Entity\ImageStyle;
+
 /**
  * Implements theme hooks for node.
  */
@@ -25,6 +28,29 @@ function unesco_oer_dc_preprocess_node(&$variables) {
         // If url is linking youtube video, prepare embed html
         if (!empty($url) && strpos($url, 'https://www.youtube.com') !== false) {
             $variables['video_embed'] = get_youtube_embed($url, 560 * 2, 315 * 2);
+        }
+    }
+
+    if (in_array($node_type, ['event', 'resource', 'news'])) {
+        $author = $node->getOwner();
+        if ($author) {
+            $variables['author_display_name'] = get_user_display_name($author);
+            if (!empty($variables['author_name'])) {
+                $variables['author_url'] = Url::fromRoute('entity.user.canonical', ['user' => $author->id()])->toString();
+            }
+
+            $picture = $author->get('user_picture')->entity;
+            $uri = $picture
+                ? ImageStyle::load('thumbnail')->buildUrl($picture->getFileUri())
+                : generate_avatar($author, 100);
+            $variables['author_picture'] = [
+                '#theme' => 'image',
+                '#uri' => $uri,
+                '#alt' => $variables['author_display_name'],
+                '#attributes' => [
+                    'class' => ['c-user__picture'],
+                ],
+            ];
         }
     }
 }
