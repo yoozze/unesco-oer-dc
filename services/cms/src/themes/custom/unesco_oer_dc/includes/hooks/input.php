1d600c4d4c1ff7fd3fd90490b7bb4c9ecd6ed665
yoozze <joze.peternelj@qlector.com>
2026-07-22 07:27:24 +0200

    Add autocomplete and multi-value field styles: introduce new SCSS files for autocomplete functionality and multi-value field widgets, enhancing user interaction and layout. Update global styles for improved consistency and add field links to event and news form templates.

--- services/cms/src/themes/custom/unesco_oer_dc/includes/hooks/input.php
+++ services/cms/src/themes/custom/unesco_oer_dc/includes/hooks/input.php
@@ -5,7 +5,11 @@
  */
 
 function unesco_oer_dc_preprocess_input__submit(&$variables) {
-    $id = $variables['element']['#id'];
+    $element = &$variables['element'];
+    $id = $element['#id'] ?? '';
+    $name = $element['#name'] ?? '';
+    $classes = $element['#attributes']['class'] ?? [];
+
     if (
         // str_starts_with($id, 'edit-submit') ||
         str_starts_with($id, 'edit-submit-news') ||
@@ -13,7 +17,15 @@ function unesco_oer_dc_preprocess_input__submit(&$variables) {
         str_starts_with($id, 'edit-submit-activities') ||
         str_starts_with($id, 'edit-submit-resources')
     ) {
-        $variables['element']['#leading_icon'] = 'search';
+        $element['#leading_icon'] = 'search';
+    }
+
+    // Secondary actions in multi-value field widgets.
+    if (
+        in_array('field-add-more-submit', $classes, TRUE)
+        || str_ends_with($name, '_remove_button')
+    ) {
+        $element['#button_variant'] = 'outlined';
     }
 }
 
